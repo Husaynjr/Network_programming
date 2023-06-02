d9b987fa371182a2c37f68564c69dc94141fd100
Husaynjr <129748985+Husaynjr@users.noreply.github.com>
2023-06-02 09:49:40 +0300

    Update question_three.py

--- network_programming_homework_1/question_three.py
+++ network_programming_homework_1/question_three.py
@@ -1,5 +1,17 @@
+import json
+grade = 0
+with open("Questions.json","r") as file:
+    d = json.load(file)
+    
+user_name = input("please enter your name first : ")
+for i in range(1,21):
+    answer = input(f"{i} : {d[str(i)][0]} : ")
+    if answer == d[str(i)][1]:
+        grade += 1
+print(f"the deserved grade is {grade}")
 try :
-    x = int(input("enter the binary number : "),base=2)
-    print (f"the decimal value is {x}")
+    with open(r"grades.txt",'a') as file:
+        file.write(user_name+" : "+str(grade)+"\n")
 except:
-    print("inserted values should contain only ones and zeroes")
+    with open(r"grades.txt","w") as file:
+        file.write(user_name+" : "+str(grade)+"\n")
